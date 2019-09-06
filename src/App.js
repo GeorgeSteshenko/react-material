0ae41e6c624dfe625e6f076c3cc9a3d11bd702fc
Steshenko Yegor <steshenkoyegor@Yegor-Steshenkos-MacBook-Pro.local>
2019-09-06 13:34:13 +0300

    About page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { getPosts } from "./services/dummy-posts";
 import Posts from "./components/posts";
 import PostPage from "./components/postPage";
 import Contact from "./components/contact";
+import About from "./components/about";
 import Home from "./components/home";
 import NotFound from "./components/notFound";
 
@@ -37,6 +38,7 @@ class App extends Component {
             render={props => <Posts posts={this.state.posts} {...props} />}
           />
           <Route path="/contact" component={Contact} />
+          <Route path="/about" component={About} />
           <Route path="/not-found" component={NotFound} />
           <Route path="/" exact component={Home} />
           <Redirect to="/not-found" />
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { List, ListItem, ListItemText, Typography } from "@material-ui/core";
 import { NavLink } from "react-router-dom";
-import { Home, Book, AccountBox } from "@material-ui/icons";
+import { Home, Book, AccountBox, Info } from "@material-ui/icons";
 
 const Navbar = () => {
   return (
@@ -21,6 +21,17 @@ const Navbar = () => {
             color="inherit"
           >
             Posts <Book />
+          </Typography>
+        </ListItemText>
+
+        <ListItemText inset>
+          <Typography
+            variant="h6"
+            component={NavLink}
+            to="/about"
+            color="inherit"
+          >
+            About <Info />
           </Typography>
         </ListItemText>
 
